fa700028e27244f0bb1bbded23b5a4c24b092ba6
Alex Webber <alexanderwebber76@gmail.com>
2024-06-07 19:12:09 -0700

    new keymaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,4 +1,17 @@
+local set = function(mode, map, cmd)
+  return vim.keymap.set(mode, map, cmd);
+end
 vim.g.mapleader = " ";
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
---vim.keymap.set("<leader>pv", ":Ex<CR>")
---vim.keymap.set("<leader>u", ":UndotreeShow<CR>")
+-- Normal
+set("n", "<leader>pv", vim.cmd.Ex) 
+set("n", "<leader>pv", ":Ex<CR>")
+set("n", "<leader>u", ":UndotreeShow<CR>")
+set("n", "J", "mzJ`z")
+-- Half page jump
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+-- V maps
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
+-- X maps 
+set("x", "<leader>p", "\"_dP")
