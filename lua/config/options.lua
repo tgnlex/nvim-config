488d6cf57ad7c29834fef6bdaeddf01206b3300e
Alex Webber <alexanderwebber76@gmail.com>
2024-06-08 23:12:27 -0700

    added some basic options

---
+++ lua/config/options.lua
@@ -0,0 +1,15 @@
+vim.opt.number = true;
+vim.opt.mouse = 'a';
+vim.opt.showmode = false;
+vim.opt.clipboard = 'unnamedplus';
+vim.opt.breakindent = true;
+vim.opt.undofile = true;
+vim.opt.ignorecase = true;
+vim.opt.smartcase = true;
+vim.opt.signcolumn = 'yes';
+vim.opt.updatetime = 250;
+vim.opt.timeoutlen = 300;
+vim.opt.splitright = true;
+vim.opt.splitbelow = true;
+vim.opt.list = true;
+
