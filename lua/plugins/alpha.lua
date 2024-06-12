eb5efff281da5ad65ddf794081c2951fdff676ce
Alex Webber <alexanderwebber76@gmail.com>
2024-06-12 14:44:48 -0700

    update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -10,7 +10,7 @@ dash.section.buttons.val = {
   btn("e", "  New file", ":ene <BAR> startinsert <CR>"),
   btn("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   btn("t", "  Find text", ":Telescope live_grep <CR>"),
-  btn("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
+  btn("c", "  Configuration", ":e ~/.config/nvim/<CR>"),
   btn("w", "Work folder", ":e ~/desktop/work/ <CR>"), 
   btn("p", "Projects folder", ":e ~/desktop/work/projects <CR>"), 
   btn("k", "Kits folder", ":e ~/desktop/work/lib/ <CR>"),
@@ -19,4 +19,4 @@ dash.section.buttons.val = {
 }
 
 dash.opts.opts.noautocmd = true;
-alpha.setup(dash.opts)
+alpha.setup(dash.opts) 
