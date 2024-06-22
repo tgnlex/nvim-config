07fa4d6905a6d9583e395c4ed87be3d3a5fe160e
tgnlex <alexanderwebber76@gmail.com>
2024-06-22 15:23:59 -0400

    update

--- init.lua
+++ init.lua
@@ -1,1 +1,22 @@
-require('main')
+-- Core -config
+require ("config.options")
+require ("config.remap")
+require ("config.plugins")
+require ("config.cmp")
+require ("config.theme")
+require ("config.autocmd")
+-- LSP CONFIG
+require("plugins.neoconf")
+require("lsp")
+-- Plugins 
+
+require ("plugins.telescope")
+require ("plugins.bufferline")
+require ("plugins.autopairs")
+require ("plugins.treesitter")
+require ("plugins.alpha")
+require ("plugins.nvimtree")
+require ("plugins.toggleterm")
+require ("plugins.harpoon")
+require ("plugins.comment")
+require ("plugins.gitsigns")
